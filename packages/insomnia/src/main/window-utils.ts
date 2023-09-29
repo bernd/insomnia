a9d32c75f7d220cc91ac660f4daa82e756e38a1f
Bernd Ahlers <bernd@tuneafish.de>
2023-09-29 14:22:25 +0200

    Update more links and change Changelog link

--- packages/insomnia/src/main/window-utils.ts
+++ packages/insomnia/src/main/window-utils.ts
@@ -392,7 +392,7 @@ export function createWindow() {
         label: 'Show Software License',
         click: () => {
           // eslint-disable-next-line no-restricted-properties
-          shell.openExternal('https://insomnia.rest/license');
+          shell.openExternal('https://github.com/bernd/insomnia/blob/main/LICENSE');
         },
       },
     ],
