5ef8e038054b23508478a29c3e70fd629f772bc1
James Gatz <jamesgatzos@gmail.com>
2023-10-04 17:07:30 +0200

    Fix: Clicking a folder for the first time should expand/collapse it. (#6642)
    * fix folder icon alignment in sidebar
    
    * init collapsed value in requestMeta
    
    * fix oauth test
    
    * fix grpc tests
    
    ---------
    
    Co-authored-by: jackkav <jackkav@gmail.com>
    (cherry picked from commit 417ed525a710a6c729b4c87ac68f2d050c5e8983)

--- packages/insomnia/src/ui/routes/debug.tsx
+++ packages/insomnia/src/ui/routes/debug.tsx
@@ -967,7 +967,7 @@ export const Debug: FC = () => {
                         )}
                         {isRequestGroup(item.doc) && (
                           <Icon
-                            className="w-6"
+                            className="w-6 flex-shrink-0"
                             icon={item.collapsed ? 'folder' : 'folder-open'}
                           />
                         )}
